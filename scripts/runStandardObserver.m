c02ec72d974d598c6a66e5064f275a177c84f41c
Jonathan Winawer <jonathan.winawer@nyu.edu>
2016-06-28 16:11:34 -0400

    change to comments

--- scripts/runStandardObserver.m
+++ scripts/runStandardObserver.m
@@ -1,31 +1,39 @@
 %%
 %%%%%%%%%% PART 1 - parameters %%%%%%%%%%
 %
-%% Load fMRI data as deposited by Noah's script
-retData = load('/Volumes/server/Projects/SOC/data/retinotopy/wl_subj001.mat');
+% epaRaw: matrix containing eccentricity (deg visual angle), polar angle
+%           (deg from upper vertical), and visual area (1-3)
+%    epa: same as epaRaw but trimmed for border voxels, etc
+%
+%% Load retinotopy template as deposited by Noah's script
+templateOutput = load('/Volumes/server/Projects/SOC/data/retinotopy/wl_subj001.mat');
 
-epaRaw = cat(2, retData.eccentricity', retData.polar_angle', retData.visual_area');
+epaRaw = cat(2, templateOutput.eccentricity', templateOutput.polar_angle', templateOutput.visual_area');
 nVoxRaw = size(epaRaw,1);
     % Out: voxs x 3 (ecc, polar angle, area)
 
 % Remove visual areas 1.5 and 2.5, which are voxels on the borders
 discardAreas = or(epaRaw(:,3) == 1.5, epaRaw(:,3) == 2.5);
+
+% Remove eccentricities that are large (we might skip this or do it later
+% for visualization)
 discardEcc = epaRaw(:,1) > 20;
 discardVoxels = or(discardAreas, discardEcc);
 epa = epaRaw(~discardVoxels, :);
     
-%% Sanity check hemifield (example/dummy)
-% Convert eccentricity and polar angle (in degrees) to x,y (in pixels)
-imSzPxDemo = 300; % images assumed to be square
-pxPerDegDemo = 10; % these are just dummy values
-xyDemo = stdObs_epToXy(epa(:,1:2), imSzPxDemo, pxPerDegDemo);
-    % In: voxs x 2 (ecc, polar angle), 1x2 (size), 1
-    % Out: voxs x 2 (x, y)
-    
-figure(1); hold on;
-scatter(xyDemo(epa(:,2) >=0, 1), xyDemo(epa(:,2) >=0, 2), 'ro');
-scatter(xyDemo(epa(:,2) < 0, 1), xyDemo(epa(:,2) < 0, 2), 'bo');
-axis ij; xlim([0, imSzPxDemo]); ylim([0, imSzPxDemo]);
+% %% Sanity check hemifield (example/dummy)
+% % Convert eccentricity and polar angle (in degrees) to x,y (in pixels)
+% imSzPxDemo = 400; % images assumed to be square
+% pxPerDegDemo = 10; % these are just dummy values
+% xyDemo = stdObs_epToXy(epa(:,1:2), imSzPxDemo, pxPerDegDemo);
+%     % In: voxs x 2 (ecc, polar angle), 1x2 (size), 1
+%     % Out: voxs x 2 (x, y)
+%     
+% figure(1); clf; hold on;
+% scatter(xyDemo(epa(:,2) >=0, 1), xyDemo(epa(:,2) >=0, 2), 'ro');
+% scatter(xyDemo(epa(:,2) < 0, 1), xyDemo(epa(:,2) < 0, 2), 'bo');
+% 
+% axis ij; xlim([0, imSzPxDemo]); ylim([0, imSzPxDemo]);
 
 %%
 %%%%%%%%%% PART 2 - predictions %%%%%%%%%%
@@ -49,7 +57,7 @@ pxPerDeg = imSzPx / totalFovDeg;
 %% Prepare parameters
 params = stdObs_epaToParams(epa, imSzPx, pxPerDeg);
     % In: voxs x 3 (ecc, polar angle, area)
-    % Out: voxs x 3 (x, y, sigma_s, n, c)
+    % Out: voxs x 5 (x, y, sigma_s, n, c)
   
 % (Sanity check: do pRF sizes get larger with area?)
 % mean(params(epa(:,3) == 1, 3)) - 24.38 (pixels)
